771c0e96048bed3f3949893adf12a6a28590d2cb
Dylan Carter <167344346+dylcarinc@users.noreply.github.com>
2026-03-13 13:33:23 -0500

    step 7

---
+++ CarterUniversity/Pages/Instructors/InstructorCoursesPageModel.cs
@@ -0,0 +1,32 @@
+using CarterUniversity.Data;
+using CarterUniversity.Models;
+using CarterUniversity.Models.SchoolViewModels;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CarterUniversity.Pages.Instructors
+{
+    public class InstructorCoursesPageModel : PageModel
+    {
+        public List<AssignedCourseData> AssignedCourseDataList;
+
+        public void PopulateAssignedCourseData(SchoolContext context,
+                                               Instructor instructor)
+        {
+            var allCourses = context.Courses;
+            var instructorCourses = new HashSet<int>(
+                instructor.Courses.Select(c => c.CourseID));
+            AssignedCourseDataList = new List<AssignedCourseData>();
+            foreach (var course in allCourses)
+            {
+                AssignedCourseDataList.Add(new AssignedCourseData
+                {
+                    CourseID = course.CourseID,
+                    Title = course.Title,
+                    Assigned = instructorCourses.Contains(course.CourseID)
+                });
+            }
+        }
+    }
+}
--- CarterUniversity/obj/Debug/net10.0/CarterUniversity.AssemblyInfo.cs
+++ CarterUniversity/obj/Debug/net10.0/CarterUniversity.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CarterUniversity")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e257e78d597c4d0fbde7562723c90b241d223950")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2630e147fe3a08e7d16623701686dfa41aaf2d50")]
 [assembly: System.Reflection.AssemblyProductAttribute("CarterUniversity")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CarterUniversity")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
